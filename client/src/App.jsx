28c261c39aeb8566dff64919cb35701e42ca297f
bmorri13 <bryanmorrison017@gmail.com>
2024-03-16 17:39:43 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  const fetchApi = async () => {
+    const response = await axios.get('http://localhost:5000/api/users')
+    console.log(response.data.users)
+    setUsers(response.data.users)
+  }
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>React & Flack Backend Test</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+          {
+          users.map((user, index) => (
+            <div key={index}> 
+            <span>{user}</span><br />
+            </div>
+          ))
+          }
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
